1e303868d44d4065d6b5b09f8e24a722e75337b2
gayashan4lk <gayashan4lk@gmail.com>
2025-11-06 23:33:22 +0530

    create a node

---
+++ LinkedList/src/main/java/org/example/LinkedList.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public class LinkedList{
+    Node head;
+}
--- LinkedList/src/main/java/org/example/Main.java
+++ LinkedList/src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        Node start = new Node(23);
+        start.next = new Node(3356);
+        start.next.next = new Node(999);
+        start.next.next.next = new Node(33);
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String startJson = gson.toJson(start);
+        System.out.println(startJson);
+
+//        LinkedList myList = new LinkedList();
+//        System.out.println(myList.head);
     }
 }
+
